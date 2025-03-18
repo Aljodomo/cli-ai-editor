007bdea056af8171dc598749be85b364a656843d
Aljodomo <26247274+Aljodomo@users.noreply.github.com>
2025-03-18 23:13:49 +0100

    refactor: test request processor

--- src/test_request_processor.go
+++ src/test_request_processor.go
@@ -8,41 +8,35 @@ import (
 
 // RequestProcessor handles the request processing and change management
 type TestRequestProcessor struct {
-	changes []FileChange
 }
 
 // ProcessRequest generates changes based on user input
-func (dp *TestRequestProcessor) ProcessRequest(request string) error {
+func (dp *TestRequestProcessor) ProcessRequest(request string) ([]FileChange, error) {
 	fmt.Println("Okay. Let me think about it.")
 
 	// Clear previous changes
-	dp.changes = []FileChange{}
+	var changes []FileChange
 
 	// get current directory where this programm was started
 	dir, err := os.Getwd()
 	if err != nil {
 		fmt.Println("Could not get current dir:", err)
-		return err
+		return nil, err
 	}
 
 	// get all files in current directory
 	files, err := GetFileNamesInDirectory(dir)
 	if err != nil {
 		fmt.Println("Could not get files in directory:", err)
-		return err
+		return nil, err
 	}
 
-	mySpecialFile := "my-special-file.txt"
+	mySpecialFile := "docs/my-special-file.txt"
 	if slices.Contains(files, mySpecialFile) {
-		dp.changes = append(dp.changes, FileChange{Operation: DELETE, FilePath: mySpecialFile, FileContent: "This is a special file"})
+		changes = append(changes, FileChange{Operation: DELETE, FilePath: mySpecialFile})
 	} else {
-		dp.changes = append(dp.changes, FileChange{Operation: CREATE, FilePath: mySpecialFile})
+		changes = append(changes, FileChange{Operation: CREATE, FilePath: mySpecialFile})
 	}
 
-	return nil
-}
-
-// GetFileChanges returns the proposed changes
-func (dp *TestRequestProcessor) GetFileChanges() []FileChange {
-	return dp.changes
+	return changes, nil
 }

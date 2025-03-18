6445d727e7777ddfe38f5891d9bb9654f98676a4
Aljodomo <26247274+Aljodomo@users.noreply.github.com>
2025-03-18 23:13:24 +0100

    fix: create file operation

--- src/basic_file_change_executer.go
+++ src/basic_file_change_executer.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"os"
+	"path/filepath"
+)
+
 type BasicFileChangeExecuter struct {
 }
 
@@ -8,7 +13,18 @@ func (b *BasicFileChangeExecuter) ExecuteFileChanges(changes []FileChange) error
 	for _, change := range changes {
 		switch change.Operation {
 		case EDIT:
+			err := WriteFileContent(change.FilePath, change.FileContent)
+			if err != nil {
+				return err
+			}
 		case CREATE:
+			dir := filepath.Dir(change.FilePath)
+			if dir != "." {
+				err := os.MkdirAll(dir, os.ModePerm)
+				if err != nil {
+					return err
+				}
+			}
 			err := WriteFileContent(change.FilePath, change.FileContent)
 			if err != nil {
 				return err
